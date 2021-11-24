581662c7228b82151c6a7aa4e0c438c07167f809
xietingting <xietingting@bosssoft.com.cn>
2021-11-24 17:26:07 +0800

    feet: 修改json错误

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,18 +3,14 @@
  * @Mail: xinlai@bosssoft.com.cn
  * @Date: 2021-11-24 16:47:49
  * @LastEditors: Comer
- * @LastEditTime: 2021-11-24 17:01:31
+ * @LastEditTime: 2021-11-24 17:24:19
  * @Description:
  */
- module.exports = {
-   extends: ["@commitlint/config-conventional"],
-   rules: {
-     "type-enum": [
-       2,
-       "always",
-       ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"]
-     ],
-      "subject-full-stop": [0, "never"],
-      "subject-case": [0, "never"]
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"]],
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"]
   }
 };
