df838a64a66854de61b599b02a6a2284613e0bf7
xietingting <xietingting@bosssoft.com.cn>
2021-11-24 16:59:01 +0800

    feet:添加git代码提交信息审查

---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: Comer
+ * @Mail: xinlai@bosssoft.com.cn
+ * @Date: 2021-11-24 16:47:49
+ * @LastEditors: Comer
+ * @LastEditTime: 2021-11-24 16:48:28
+ * @Description:
+ */
+ module.exports = {
+   extends: ["@commitlint/config-conventional"],
+   rules: {
+     "type-enum": [
+       2,
+       "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"]
+    ],
+      "subject-full-stop": [0, "never"],
+     "subject-case": [0, "never"]
+    
+  }
+};
